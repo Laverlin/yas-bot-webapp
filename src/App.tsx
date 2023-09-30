9b712bee1fdd928cfbb1ae9f4ea5aa36a8f77eb9
Laverlin <ilaverlin@gmail.com>
2023-10-01 01:16:13 +0200

    add telegramm objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import { useState, useEffect }  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TelegramWebApps } from 'telegram-webapps-types';
+
+
+
+//const tele = window.Telegram.WebApp;
+
+const teleApp = (window as any).Telegram?.WebApp as TelegramWebApps.WebApp;
+
+
+export interface ITelegramContext {
+  webApp?: TelegramWebApps.WebApp;
+  user?: TelegramWebApps.WebAppUser;
+}
+
+
 
 function App() {
+  useEffect(() => {
+    teleApp.ready();
+  });
+
+  const teleUser = teleApp.initDataUnsafe.user;
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ID: 123456
+          ID: {teleUser?.id}
         </p>
 
       </header>
